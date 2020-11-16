4cacd455675888adb58d5021695f332be475c496
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-16 07:27:42 -0800

    Example of new input system

---
+++ Assets/Scripts/ExampleMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Input : MonoBehaviour
+{
+
+    PlayerMovement player;
+    [SerializeField] private float speed;
+
+    private void Awake() {
+        player = new PlayerMovement();
+    }
+
+    public void Update() {
+        Vector2 movement = player.Player.Move.ReadValue<Vector2>();
+
+        Vector3 currPosition = transform.position;
+        currPosition.x += movement.x * speed * Time.deltaTime;
+        transform.position = currPosition;
+    }
+
+    private void OnEnable() {
+        player.Enable();
+    }
+
+    private void OnDisable() {
+        player.Disable();
+    }
+
+}
