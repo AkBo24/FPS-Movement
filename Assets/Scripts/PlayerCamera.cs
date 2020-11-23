c9e7e7101c46a08c1f88197f41255cfbdeb0756d
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-23 09:45:00 -0800

    loging cursor movements

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    [SerializeField] MovementMaster.MovementMaster movementMaster;
+
+    private void Awake() {
+        movementMaster = new MovementMaster.MovementMaster();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Keeps cursor on the camera
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(movementMaster.Player.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable() {
+        movementMaster.Enable();
+    }
+
+    private void OnDisable() {
+        movementMaster.Disable();
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,12 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private MovementMaster.MovementMaster movementMaster;
     [SerializeField] private CharacterController playerController;
-    [SerializeField] private float movementSpeed;
+    [SerializeField] private float movementSpeed, velocity = 9.81f;
+    private bool isGrounded;
+
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    [SerializeField] private float groundDistance;
 
     void Awake()
     {
@@ -17,8 +22,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log(movementMaster.Player.Move.ReadValue<Vector2>());
-
         float x = movementMaster.Player.Move.ReadValue<Vector2>()[0];
         float z = movementMaster.Player.Move.ReadValue<Vector2>()[1];
         Vector3 move = transform.right*x + transform.forward*z;
